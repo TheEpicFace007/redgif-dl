c82e7a94913d5e2f47a0ca6bbc9dcb9000a9c1b9
Vini Dalvino <vinidalvino@github.com>
2020-11-08 12:04:14 -0500

    Updated test with assertion

--- test.js
+++ test.js
@@ -1,9 +1,13 @@
 const dlRedgif = require("./index");
+const fs = require("fs")
 
-dlRedgif("https://www.redgifs.com/watch/newdrearyblackwidowspider", "riley reid blow job")
+dlRedgif("https://www.redgifs.com/watch/newdrearyblackwidowspider", "riley reid blow job", "", true)
     .then((video) => {
         console.log(`High quality downloaded video: ${video.HQ}`);
         console.log(`Low quality downloaded video: ${video.LQ}`);
+
+        console.assert(fs.existsSync(video.LQ), "The high quality video does not exist");
+        console.assert(fs.existsSync(video.HQ), "The low quality video does not exist")
     })
     .catch((reason) => {
         console.log(`Failed to download the video due to ${reason}`)
